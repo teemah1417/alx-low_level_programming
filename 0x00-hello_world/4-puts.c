09fb74e875e8d8ce98c8b1428e50847ee016d21b
teemah1417 <alayatoheeb1998@yahoo.com>
2022-09-10 05:58:31 -0700

    tab

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
- * main -prints statement with puts function
- */
+* main -prints statement with puts function
+*/
 int main(void)
 {
-        puts("\"Programming is like building a multilingual puzzle");
-        return (0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }

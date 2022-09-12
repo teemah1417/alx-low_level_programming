6cbbf632aaebde272c1dff908ed83dfe782b4d64
teemah1417 <alayatoheeb1998@yahoo.com>
2022-09-12 04:53:34 -0700

    update

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -7,7 +7,8 @@
  *
  * Return: Always 0 (Success)
 */
-	int main(void)
+
+int main(void)
 {
 	int digit = 0;
 
@@ -17,7 +18,7 @@
 		putchar(digit + '0');
 		++digit;
 	}
-	putchar("\n");
+	putchar('\n');
 
 	return (0);
 }
